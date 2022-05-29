5058c780ef216bac2914c21c15367a72e8249c6c
JS-3 <106467278+JS-3@users.noreply.github.com>
2022-05-29 05:57:50 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+// My Age 
+
+const myAge = 33
+
+// Changing variable of 2 
+
+var earlyYears = 2
+
+earlyYears = earlyYears * 10.5
+
+// My age - 2 multiplied by 4
+
+const laterYears = (myAge - 2) * 4
+
+// Adds the variables to equal my age in dog years 
+
+myAgeInDogYears = earlyYears + laterYears
+
+// Writes my name in lower case letters 
+
+const myName = 'JUSTIN'.toLowerCase()
+
+// My age in human and dog years 
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
